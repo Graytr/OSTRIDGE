0496d16adcc73c7ea38eb2edbd340f773eff6851
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2019-01-02 18:21:50 -0600

    Added more handler functions

--- ostridge/src/interrupts.rs
+++ ostridge/src/interrupts.rs
@@ -22,29 +22,29 @@ pub static PICS: spin::Mutex<ChainedPics> =
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
-        // TODO: divide by zero
-        // TODO: debug
-        // TODO: Non-maskable
+        idt.divide_by_zero.set_handler_fn(divide_by_zero_handler);
+        idt.debug.set_handler_fn(debug_handler);
+        idt.non_maskable_interrupt.set_handler_fn(non_maskable_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        // TODO: Overflow
-        // TODO: Bound Range Exceeded
-        // TODO: Invalid Opcode
-        // TODO: Device Not Available
+        idt.overflow.set_handler_fn(overflow_handler);
+        idt.bound_range_exceeded.set_handler_fn(bound_range_exceeded_handler);
+        idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
+        idt.device_not_available.set_handler_fn(device_not_available_handler);
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-        // TODO: Invalid TSS
-        // TODO: Segment Not Present
-        // TODO: Stack-Segment Fault
-        // TODO: General Protection Fault
-        // TODO: Page Fault
-        // TODO: x87 Floating Point Exception
-        // TODO: Alignment Check
-        // TODO: Machine Check
-        // TODO: SIMD Floating Point Exception
-        // TODO: Virtualization Exception
-        // TODO: Security Exception
+        // idt.invalid_tss.set_handler_fn(invalid_TSS_handler); TODO: FIX THIS
+        // idt.segment_not_present.set_handler_fn(segment_not_present_handler); TODO: FIX THIS
+        // idt.stack_segment_fault.set_handler_fn(stack_segment_fault_handler); TODO: FIX THIS
+        // idt.general_protection_fault.set_handler_fn(general_protection_fault_handler); TODO: FIX THIS
+        // idt.page_fault.set_handler_fn(page_fault_handler); TODO: FIX THIS
+        idt.x87_floating_point.set_handler_fn(x87_floating_point_handler);
+        // idt.alignment_check.set_handler_fn(alignment_check_handler); TODO: FIX THIS
+        idt.machine_check.set_handler_fn(machine_check_handler);
+        idt.simd_floating_point.set_handler_fn(simd_floating_point_handler);
+        idt.virtualization.set_handler_fn(virtualization_handler);
+        //idt.security_exception.set_handler_fn(security_exception_handler); TODO: FIX THIS
         // TODO: Triple Fault
         idt[usize::from(TIMER_INTERRUPT_ID)]
             .set_handler_fn(timer_interrupt_handler); // Add the timer interrupt
@@ -66,15 +66,105 @@ pub fn init_idt() {
     IDT.load();
 }
 
+/// Exception handler for divide by zero
+extern "x86-interrupt" fn divide_by_zero_handler(stack_frame: &mut ExceptionStackFrame) {
+    println!("EXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
+}
+
+/// Exception handler for debug exceptions
+extern "x86-interrupt" fn debug_handler(stack_frame: &mut ExceptionStackFrame) {
+    println!("EXCEPTION: DEBUG\n{:#?}", stack_frame);
+}
+
+/// Exception handler for non-maskable exceptions
+extern "x86-interrupt" fn non_maskable_handler(stack_frame: &mut ExceptionStackFrame) {
+    println!("EXCEPTION: NON-MASKABLE\n{:#?}", stack_frame);
+}
+
 /// Add an exception handler for breakpoints
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+/// Exception handler for overflow exceptions
+extern "x86-interrupt" fn overflow_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: OVERFLOW\n{:#?}", stack_frame);
+}
+
+/// Bound Range Exceeded exception handler
+extern "x86-interrupt" fn bound_range_exceeded_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: BOUND RANGE EXCEEDED\n{:#?}", stack_frame);
+}
+
+/// Invalid Opcode handler
+extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: INVALID OPCODE\n{:#?}", stack_frame);
+}
+
+/// Device not available handler
+extern "x86-interrupt" fn device_not_available_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: DEVICE NOT AVAILABLE\n{:#?}", stack_frame);
 }
 
 /// Add an exception handler for double faults, this prevents triple faults
 extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackFrame, _error_code: u64){
     println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
     hlt_loop();
+}
+
+/// Invalid TSS handler
+extern "x86-interrupt" fn invalid_TSS_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: INVALID TSS\n{:#?}", stack_frame);
+}
+
+/// Segment not present handler
+extern "x86-interrupt" fn segment_not_present_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: SEGMENT NOT PRESENT\n{:#?}", stack_frame);
+}
+
+/// Stack Segment Fault handler
+extern "x86-interrupt" fn stack_segment_fault_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: STACK SEGMENT FAULT\n{:#?}", stack_frame);
+}
+
+/// General Protection Fault Handler
+extern "x86-interrupt" fn general_protection_fault_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: GENERAL PROTECTION FAULT\n{:#?}", stack_frame);
+}
+
+/// Page Fault Handler
+extern "x86-interrupt" fn page_fault_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: PAGE FAULT\n{:#?}", stack_frame);
+}
+
+/// x87 Floating Point Handler
+extern "x86-interrupt" fn x87_floating_point_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: x87 FLOATING POINT\n{:#?}", stack_frame);
+}
+
+/// Alignment Check handler
+extern "x86-interrupt" fn alignment_check_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: ALIGNMENT CHECK\n{:#?}", stack_frame);
+}
+
+/// Machine Check Handler
+extern "x86-interrupt" fn machine_check_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: MACHINE CHECK\n{:#?}", stack_frame);
+}
+
+/// simd Floating Point Handler
+extern "x86-interrupt" fn simd_floating_point_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: simd FLOATING POINT\n{:#?}", stack_frame);
+}
+
+/// virtualization Handler
+extern "x86-interrupt" fn virtualization_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: VIRTUALIZATION\n{:#?}", stack_frame);
+}
+
+/// Security Exception Handler
+extern "x86-interrupt" fn security_exception_handler(stack_frame: &mut ExceptionStackFrame){
+    println!("EXCEPTION: SECURITY EXCEPTION\n{:#?}", stack_frame);
 }
 
 /// Interrupt handler for timer
